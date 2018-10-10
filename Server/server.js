295e9740fa15176266fde3e8706fc7f5c9d63455
Chris Jonathan <christophereko@hotmail.fr>
2018-10-10 21:01:36 +0100

    More animation work and also working on making the logo clickable.

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 const favicon = require('serve-favicon');
+const app = express();
 const port = 3000;
 
 app.use(favicon(path.join(__dirname, '../public/img', 'logo.ico')));
