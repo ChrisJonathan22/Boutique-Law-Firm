4ea38636b20e0ec69ecb2528eb5fdcfac366339f
Chris Jonathan <christophereko@hotmail.fr>
2018-11-01 23:00:33 +0000

    Working on the mobile side menu.

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,9 @@
 let menu = document.getElementById('menu-container');
+let sideMenu = document.getElementById('side-menu');
 
 function burgerMenu () {
+   
+    sideMenu.classList.toggle('show');
     this.classList.toggle('change');
 }
 menu.addEventListener('click', burgerMenu, false);
